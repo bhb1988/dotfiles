7e7770c2b45a431158587f7ac9f335efa0cb2293
DaSea <dhf0214@126.com>
2018-01-06 17:25:02 +0800

    上传zsh等

---
+++ mshell/zsh/plugins/exa/exa.plugin.zsh
@@ -0,0 +1,11 @@
+# alias
+alias ls='exa'
+
+alias kl='exa -lh'
+alias ks='exa -G'
+alias kt='exa -T'
+alias ka='exa -a'
+alias kla='exa -lah'
+alias kd='exa -d'
+alias kg='exa --git'
+alias la='exa -a'
